d4497925dbfc7048c431004018a9f20cc486d075
sherfuddin khan <ferozekhanmohd9@gmail.com>
2026-08-10 11:13:38 +0530

    done all

--- src/pages/ProductInventory/ProductInventoryFilters.jsx
+++ src/pages/ProductInventory/ProductInventoryFilters.jsx
@@ -1,33 +1,16 @@
 import React from "react";
-
-import {
-    Paper,
-    Grid,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem,
-    TextField
-} from "@mui/material";
+import {Paper,Grid,FormControl,InputLabel,Select,MenuItem,TextField} from "@mui/material";
 
 
 const ProductInventoryFilters = ({
-
     statusFilter,
-
     setStatusFilter,
-
     quantityFilter,
-
     setQuantityFilter,
-
     inventories = []
-
 }) => {
 
-
     const quantityOptions = [
-
         {
             label: "All",
             value: ""
@@ -53,231 +36,102 @@ const ProductInventoryFilters = ({
 
 
     return (
-
         <Paper
-
             elevation={2}
-
             sx={{
-
                 p:2,
-
                 mb:3
-
             }}
 
         >
-
             <Grid
-
                 container
-
                 spacing={2}
-
             >
-
-
                 <Grid
-
                     item
-
                     xs={12}
-
                     md={4}
-
                 >
-
                     <FormControl
-
                         fullWidth
-
                     >
-
                         <InputLabel>
-
                             Status
-
                         </InputLabel>
-
-
                         <Select
-
-                            value={
-                                statusFilter
-                            }
-
+                            value={statusFilter}
                             label="Status"
-
                             onChange={(e)=>
-
-                                setStatusFilter(
-
-                                    e.target.value
-
-                                )
-
+                                setStatusFilter(e.target.value)
                             }
-
                         >
-
                             <MenuItem value="All">
-
                                 All
-
                             </MenuItem>
-
-
                             <MenuItem value="Active">
-
                                 Active
-
                             </MenuItem>
-
-
                             <MenuItem value="Inactive">
-
                                 Inactive
-
                             </MenuItem>
-
-
                         </Select>
-
-
                     </FormControl>
-
-
                 </Grid>
-
-
-
                 <Grid
-
                     item
-
                     xs={12}
-
                     md={4}
-
                 >
-
                     <FormControl
-
                         fullWidth
-
                     >
-
                         <InputLabel>
-
                             Stock Level
-
                         </InputLabel>
-
-
                         <Select
-
-                            value={
-                                quantityFilter
-                            }
-
+                            value={ quantityFilter}
                             label="Stock Level"
-
                             onChange={(e)=>
-
-                                setQuantityFilter(
-
-                                    e.target.value
-
-                                )
-
+                                setQuantityFilter(e.target.value)
                             }
-
                         >
-
-
                             {
-
                                 quantityOptions.map(
-
                                     option => (
-
                                         <MenuItem
-
-                                            key={
-                                                option.value
-                                            }
-
-                                            value={
-                                                option.value
-                                            }
-
+                                            key={option.value}
+                                            value={option.value}
                                         >
-
                                             {
                                                 option.label
                                             }
-
                                         </MenuItem>
-
                                     )
-
                                 )
-
                             }
-
-
                         </Select>
-
-
                     </FormControl>
-
-
                 </Grid>
-
-
-
                 <Grid
-
                     item
-
                     xs={12}
-
                     md={4}
-
                 >
-
                     <TextField
-
                         fullWidth
-
                         label="Filter Info"
-
                         value={
-
                             inventories.length +
-
                             " Records"
-
                         }
-
                         InputProps={{
-
                             readOnly:true
-
                         }}
-
                     />
-
-
                 </Grid>
-
-
             </Grid>
-
-
         </Paper>
-
-
     );
 
 };
