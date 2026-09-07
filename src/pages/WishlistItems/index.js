92c5ac1ac452edf14af63464a57cce74218179c6
sherfuddin khan <ferozekhanmohd9@gmail.com>
2026-09-07 15:00:50 +0530

    111

---
+++ src/pages/WishlistItems/index.js
@@ -0,0 +1,40 @@
+
+export { default as DeleteWishlistItemDialog }
+    from "./DeleteWishlistItemDialog";
+
+export { default as WishlistItemCard }
+    from "./WishlistItemCard";
+
+export { default as WishlistItemList }
+    from "./WishlistItemList";
+
+export { default as WishlistItemModal }
+    from "./WishlistItemModal";
+
+export { default as WishlistItemPagination }
+    from "./WishlistItemPagination";
+
+export { default as WishlistItemSearch }
+    from "./WishlistItemSearch";
+
+export { default as WishlistItemStatistics }
+    from "./WishlistItemStatistics";
+
+export { default as WishlistItemTable }
+    from "./WishlistItemTable";
+
+export { default as WishlistItemToolbar }
+    from "./WishlistItemToolbar";
+
+export { default as WishlistItemView }
+    from "./WishlistItemView";
+
+export { default as WishlistItemsDetails }
+    from "./WishlistItemsDetails";
+
+export { default as WishlistItemsCreate }
+    from "./WishlistItemsCreate";
+
+export { default as WishlistItemsEdit }
+    from "./WishlistItemsEdit";
+
